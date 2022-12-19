ca633ee5ad805577c09ef191471427d9f2dc752c
tolgaisik-jotform <39388550+tolgaisik@users.noreply.github.com>
2022-12-19 11:54:32 +0300

    gltfjsx add

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -4,9 +4,8 @@ import { OrbitControls, Preload } from '@react-three/drei'
 export default function Scene({ children, ...props }) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas {...props}>
-      <directionalLight intensity={0.75} />
-      <ambientLight intensity={0.75} />
+    <Canvas {...props} style={{backgroundColor: "black"}}>
+      <ambientLight intensity={0} />
       {children}
       <Preload all />
       <OrbitControls />
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,21 +7,9 @@ import '@/styles/index.css'
 const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: true })
 
 export default function App({ Component, pageProps = { title: 'index' } }) {
-  const ref = useRef()
   return (
-    <>
-      <Header title={pageProps.title} />
-      <Layout ref={ref}>
-        <Component {...pageProps} />
-        {/* The canvas can either be in front of the dom or behind. If it is in front it can overlay contents.
-         * Setting the event source to a shared parent allows both the dom and the canvas to receive events.
-         * Since the event source is now shared, the canvas would block events, we prevent that with pointerEvents: none. */}
-        {Component?.canvas && (
-          <Scene className='pointer-events-none' eventSource={ref} eventPrefix='client'>
+    <Scene className='pointer-events-none' eventPrefix='client'>
             {Component.canvas(pageProps)}
-          </Scene>
-        )}
-      </Layout>
-    </>
+    </Scene>
   )
 }
